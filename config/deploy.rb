f087c53d426aed1fe2df1fcd6262b5830596bb32
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-07-19 16:30:07 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 lock '3.14.1'
 
 set :application, 'fleamarket_sample_75c'
-set :repo_url,  'git@github.com:Takumi0126/fleamarket_sample_75c'
+set :repo_url,  'git@github.com:Takumi0126/fleamarket_sample_75c.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-set :ssh_options, auth_methods: ['deploytest'],
+set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/deploytest.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }

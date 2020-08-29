025bde8d5ef6eb08558eea6cf9b1fa0d45be1e75
gakitomo <tmo8002@gmail.com>
2020-08-29 13:46:15 +0900

    Add Cards_Logomark

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth, if: :production?
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected  
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
   end
 
   private
 
-  def production?
-    Rails.env.production?
-  end
+  # def production?
+  #   Rails.env.production?
+  # end
   
   # def basic_auth
   #   authenticate_or_request_with_http_basic do |username, password|

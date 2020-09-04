56645e08b9776f701ddc12634400aa4caa4bc056
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-09-04 18:01:49 +0900

    Edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected  
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday])
   end
 
   private

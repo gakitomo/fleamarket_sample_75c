85eca122d249fbcd6a3e8368856780f00ed27a20
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-09-06 05:15:25 +0900

    add association

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_burden
   belongs_to_active_hash :shipping_method
   belongs_to :user, foreign_key: 'user_id'
+  belongs_to :category
 end

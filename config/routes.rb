f3635ac5b8c9afc1c36f2d12afcaba72da2e05ef
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-09-07 03:28:41 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-  resources :items, only:[:index, :new, :show]
+  resources :items, only:[:index, :new, :show, :destroy]
   resources :users
   
 

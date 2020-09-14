b0cfcfafbbf5b232fb0376559792570e9009df81
akito-i <a-imamura0827@gmail.com>
2020-09-14 21:13:25 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-    @category_parent_array = Category.where(ancestry: nil)
   end
 
   def show

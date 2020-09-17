248ecb2f4bd49537ea07870449c2cf6f47d2d12a
gakitomo <tmo8002@gmail.com>
2020-09-17 19:23:59 +0900

    reset index views

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,19 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-  resources :items do
-  collection do
-    get 'get_category_children', defaults: { format: 'json' }
-    get 'get_category_grandchildren', defaults: { format: 'json' }
+  resources :items ,:purchase do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+    member do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
   end
-  member do
-    get 'get_category_children', defaults: { format: 'json' }
-    get 'get_category_grandchildren', defaults: { format: 'json' }
-  end
-end
 
-  resources :users
+  resources :users do
+  end
   resources :cards, only: [:index, :new, :create, :show, :destroy] do
   end
   

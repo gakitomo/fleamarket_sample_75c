7b47c931f1f4d6a28cb739b82ea6f7610c149694
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2020-09-19 14:15:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+    :sessions => 'users/sessions',
   } 
+
+  devise_scope :user do
+    get 'address', to: 'users/registrations#new_address'
+    post 'address', to: 'users/registrations#create_address'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
